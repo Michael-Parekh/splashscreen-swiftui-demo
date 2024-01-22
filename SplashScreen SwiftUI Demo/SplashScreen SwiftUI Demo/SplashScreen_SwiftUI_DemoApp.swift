96ed6ea3c60111429685b2ae911577f4bdd70932
Michael Parekh <mparekh4@illinois.edu>
2024-01-21 22:57:29 -0600

    init project

---
+++ SplashScreen SwiftUI Demo/SplashScreen SwiftUI Demo/SplashScreen_SwiftUI_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreen_SwiftUI_DemoApp.swift
+//  SplashScreen SwiftUI Demo
+//
+//  Created by Michael Parekh on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreen_SwiftUI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
